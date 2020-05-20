8e522bd2404fb13a121b20b70a6cc929712a52f5
Germán Asencio <germanasencioanton@gmail.com>
2020-05-20 14:01:42 +0200

    Missing fields added.

---
+++ produ/templates/python.py
@@ -0,0 +1,11 @@
+# este codigo controla los errores de campos faltantes
+from flask import render_template
+
+
+def process_missingFields(campos, next_page):
+    """
+    :param campos: Lista de Campos que faltan
+    :param next_page: ruta al pulsar botón continuar
+    :return: plantilla generada
+    """
+    return render_template("missingFields.html", inputs=campos, next=next_page)
